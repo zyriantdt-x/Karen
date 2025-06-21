a16c43aa48692e3d1ebbeeb8083ffbb37f6be266
zyriantdt-x <me@ellis.pw>
2025-06-21 13:20:38 +0100

    change composer

--- src/Karen.Common/Abstractions/ComposerBase.cs
+++ src/Karen.Common/Abstractions/ComposerBase.cs
@@ -1,4 +1,5 @@
 ﻿using Karen.Common.Encoding;
+using System.Buffers;
 using System.IO.Pipelines;
 
 namespace Karen.Common.Abstractions;
@@ -7,40 +8,40 @@ public abstract class ComposerBase {
 
     public abstract short Header { get; }
 
-    protected async Task Write( object obj, bool as_object = false ) {
+    protected void Write( object obj, bool as_object = false ) {
         if( as_object ) {
-            await this.WriteString( obj.ToString() ?? String.Empty, append_terminator: false );
+            this.WriteString( obj.ToString() ?? String.Empty, append_terminator: false );
             return;
         }
 
         switch( obj ) {
             case string s:
-                await this.WriteString( s );
+                this.WriteString( s );
                 break;
             case int i:
-                _ = await this.writer!.WriteAsync( VL64Encoding.Encode( i ) );
+                this.writer!.Write( VL64Encoding.Encode( i ) );
                 break;
             case bool b:
-                _ = this.writer!.WriteAsync( VL64Encoding.Encode( b ? 1 : 0 ) );
+                this.writer!.Write( VL64Encoding.Encode( b ? 1 : 0 ) );
                 break;
             default:
-                await this.WriteString( obj.ToString() ?? String.Empty );
+                this.WriteString( obj.ToString() ?? String.Empty );
                 break;
         }
     }
 
-    protected async Task WriteKeyValue( object key, object value ) {
-        await this.WriteString( $"{key}:{value}" );
-        _ = await this.writer!.WriteAsync( new byte[] { 13 } ); // delimiter
+    protected void WriteKeyValue( object key, object value ) {
+        this.WriteString( $"{key}:{value}" );
+        this.writer!.Write( new byte[] { 13 } ); // delimiter
     }
 
-    protected async Task WriteValue( object key, object value ) {
-        await this.WriteString( $"{key}={value}" );
-        _ = await this.writer!.WriteAsync( new byte[] { 13 } ); // delimiter
+    protected void WriteValue( object key, object value ) {
+        this.WriteString( $"{key}={value}" );
+        this.writer!.Write( new byte[] { 13 } ); // delimiter
     }
 
-    protected async Task WriteDelimiter( object key, object value, string? delim = null ) {
-        await this.WriteString( $"{key}{delim ?? ""}{value}", append_terminator: false );
+    protected void WriteDelimiter( object key, object value, string? delim = null ) {
+        this.WriteString( $"{key}{delim ?? ""}{value}", append_terminator: false );
     }
 
     /*public override string ToString() {
@@ -53,21 +54,20 @@ public abstract class ComposerBase {
         return content;
     }*/
 
-    protected async Task WriteString( string value, bool append_terminator = true ) {
-        _ = await this.writer!.WriteAsync( System.Text.Encoding.UTF8.GetBytes( value ) );
+    protected void WriteString( string value, bool append_terminator = true ) {
+        this.writer!.Write( System.Text.Encoding.UTF8.GetBytes( value ) );
         if( append_terminator )
-            _ = await this.writer!.WriteAsync( new byte[] { 2 } );
+            this.writer!.Write( new byte[] { 2 } );
     }
 
     public async Task WriteToPipeAsync( PipeWriter writer, bool with_flush ) {
         this.writer = writer;
 
-        await this.writer!.WriteAsync( Base64Encoding.Encode( this.Header, 2 ) ); // write the header
+        this.writer!.Write( Base64Encoding.Encode( this.Header, 2 ) ); // write the header
 
         await this.ComposeAsync(); // write the contents
 
-        await this.writer!.WriteAsync( new( [1] ) ); // wtf ? write the end
-
+        this.writer!.Write( new( [1] ) ); // wtf ? write the end
 
         if( with_flush ) _ = await writer.FlushAsync();
     }
