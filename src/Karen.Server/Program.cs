1353400bd672decb45ce3b3d4e2ee41496313f0f
Ellis <me@ellis.pw>
2025-06-21 04:53:49 -0700

    make logs small

--- src/Karen.Server/Program.cs
+++ src/Karen.Server/Program.cs
@@ -11,4 +11,13 @@ builder.AddTcpService();
 
 builder.Services.AddV14();
 
+// logs
+builder.Services.AddLogging( logging => {
+    logging.ClearProviders();
+    logging.AddSimpleConsole( options => {
+        options.SingleLine = true;
+        options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+    } );
+} );
+
 await builder.Build().RunAsync();
