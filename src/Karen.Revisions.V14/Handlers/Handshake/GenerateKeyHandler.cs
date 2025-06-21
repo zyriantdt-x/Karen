ae4ab6b17c2884205374d14724d09d6a406c8e12
Ellis <me@ellis.pw>
2025-06-21 13:51:28 -0700

    oops

--- src/Karen.Revisions.V14/Handlers/Handshake/GenerateKeyHandler.cs
+++ src/Karen.Revisions.V14/Handlers/Handshake/GenerateKeyHandler.cs
@@ -6,7 +6,7 @@ public class GenerateKeyHandler : IHandler {
     public short Header => 202;
 
     public async Task HandleAsync( IKarenClient client, object body ) {
-        await client.SendAsync( new CryptoParametersComposer() );
+        await client.SendAsync( new SessionParametersComposer() );
         await client.SendAsync( new AvailableSetsComposer() );
     }
 }
