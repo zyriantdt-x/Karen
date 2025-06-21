aed3c9e2eeab90a228272622cba7575639ddcd80
Ellis <me@ellis.pw>
2025-06-21 04:52:56 -0700

    add header

--- src/Karen.Revisions.V14/Handlers/Handshake/InitCryptoHandler.cs
+++ src/Karen.Revisions.V14/Handlers/Handshake/InitCryptoHandler.cs
@@ -3,6 +3,8 @@ using Karen.Revisions.V14.Composers.Handshake;
 
 namespace Karen.Revisions.V14.Handlers.Handshake;
 public class InitCryptoHandler : IHandler {
+    public short Header => 206;
+
     public async Task HandleAsync( IKarenClient client, object body ) {
         await client.SendAsync( new CryptoParametersComposer() );
     }
