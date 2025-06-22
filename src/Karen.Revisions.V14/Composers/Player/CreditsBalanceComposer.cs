d07124b1c7fcb0c23ac2393f3a007158d6661c7c
zyriantdt-x <me@ellis.pw>
2025-06-22 14:26:56 +0100

    add get credits

---
+++ src/Karen.Revisions.V14/Composers/Player/CreditsBalanceComposer.cs
@@ -0,0 +1,10 @@
+﻿namespace Karen.Revisions.V14.Composers.Player;
+public class CreditBalanceComposer : ComposerBase {
+    public override short Header => 6;
+
+    public required int Credits { get; init; }
+
+    protected override void Compose() {
+        this.Write( this.Credits + ".0" );
+    }
+}
---
+++ src/Karen.Revisions.V14/Handlers/Player/GetCreditsHandler.cs
@@ -0,0 +1,11 @@
+﻿using Karen.Common.Interfaces;
+using Karen.Revisions.V14.Composers.Player;
+
+namespace Karen.Revisions.V14.Handlers.Player;
+public class GetCreditsHandler : IHandler {
+    public short Header => 8;
+
+    public async Task HandleAsync( IKarenClient client, object body ) {
+        await client.SendAsync( new CreditBalanceComposer() { Credits = client.Player!.PlayerDetails.Credits } );
+    }
+}
