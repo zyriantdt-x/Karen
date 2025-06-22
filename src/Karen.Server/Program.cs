4efbd59ca63831c8e8ea8e6b23ae73b3657aabdd
zyriantdt-x <me@ellis.pw>
2025-06-22 10:56:00 +0100

    fix formatting

--- src/Karen.Server/Program.cs
+++ src/Karen.Server/Program.cs
@@ -11,13 +11,12 @@ builder.AddTcpService();
 
 builder.Services.AddV14();
 
-// logs
-builder.Services.AddLogging( logging => {
-    logging.ClearProviders();
-    logging.AddSimpleConsole( options => {
-        options.SingleLine = true;
-        options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
-    } );
-} );
+// logs - this is ugly asf
+builder.Services.AddLogging( logging => 
+            _ = logging.ClearProviders()
+                .AddSimpleConsole( options => {
+                    options.SingleLine = true;
+                    options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+                } ) );
 
 await builder.Build().RunAsync();
